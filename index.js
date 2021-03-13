491d5043d40b4a2ea72f0b40ae3996ccebc6dbaa
jLuseno161 <joyluseno2@gmail.com>
2021-03-13 21:34:26 +0300

    Array for storing data

---
+++ index.js
@@ -0,0 +1,11 @@
+//array for storing milk and shade data
+var sheds  = [
+    {   shedName :'Shed A',
+    },
+    {   shedName :'Shed B',
+    },
+    {   shedName :'Shed C',
+    },
+    {   shedName :'Shed D',
+    }
+];
