b3921aa1da5396f108fc34a04f827749e1cd96a6
jLuseno161 <joyluseno2@gmail.com>
2021-03-13 22:53:54 +0300

    Add production per day function

--- index.js
+++ index.js
@@ -27,3 +27,44 @@ function add_sheds(){
     alert(newObject.shedName+ " added successfully");
     document.getElementById("inputText").value= ' ';
 }
+//total production
+var milkPerShed = 0;
+var total = 0;
+function totalProduction(){
+      
+    document.getElementById("Report").innerHTML =' '; 
+      var i = 0;
+        while (i < sheds.length) {
+          //text += "<br>The number is " + i;
+          var ltrspershed = prompt("Please Litres of milk from:" +sheds[i].shedName, "");
+            if(ltrspershed == null){
+                alert("You Pressed Cancel."); 
+                break; 
+              }
+            else{
+                ltrspershed = parseInt(ltrspershed);
+                if ( isNaN(ltrspershed)){ 
+                    alert("Enter a valid figure.");
+                    document.getElementById("Report").innerHTML = ' ';
+                    break; 
+                  }
+                else { 
+                    obj = {};
+                    sheds.map(function(obj){
+                    obj.milkPerShed = ltrspershed;
+                    return obj;
+                    });
+                }
+                
+               document.getElementById("Report").innerHTML += "Your production in " + sheds[i].shedName + " is " + sheds[i].milkPerShed + " litres per day."+ "<br>" +"<br>";
+               milkPerShed += parseFloat(sheds[i].milkPerShed);            
+              }
+          i++;
+        }
+              total = milkPerShed;
+              document.getElementById("Report").innerHTML += "The total production is " +  total + " litres per day" + "<br>" +"<br>" ; 
+
+              incomeOverTime(price, period);
+
+              incomePerMonth();
+}
