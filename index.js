84db83ddae4086ab4ec5fc006130afb6db19da78
jLuseno161 <joyluseno2@gmail.com>
2021-03-14 11:04:44 +0300

    object for storing months

--- index.js
+++ index.js
@@ -87,3 +87,8 @@ function incomeOverTime(price, period){
         document.getElementById("Report").innerHTML +=  "Your " + period[j].name +" income will be Ksh. "+ income +"<br>"+"<br>"; 
     }  
 };
+
+var month = { 
+  January : 31, February: 29,March:31,April:30,May: 31,June:30,July:31,
+  August:31,September:30,October:31, November:30, December:31
+};
